c167ae4c19bd2e536ad1fb5531bcae64a22c6d73
gertd <gert.drapers@live.com>
2017-01-24 16:11:13 -0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # SqlBatchReader
-Reader for .SQL files, separating batches, stripping comments, etc.
+.NET StreamReader for .SQL files to separate batches, stripping comments, and in the future to resolve SQLCMD variables and commands etc.
---
+++ SqlBatchReaderCmd/Program.cs
@@ -0,0 +1,61 @@
+﻿using SqlBatchReader;
+using SqlBatchReaderCmd.Common;
+using System;
+using System.IO;
+
+namespace SqlBatchReaderCmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var parsedArgs = new AppArgs();
+            if (Parser.ParseArguments(args, parsedArgs))
+            {
+                if (parsedArgs.Version || parsedArgs.Help)
+                {
+                    if (parsedArgs.Version)
+                    {
+                        Helper.DisplayVersion();
+                    }
+                    else if (parsedArgs.Help)
+                    {
+                        Console.Out.Write(Parser.ArgumentsUsage(typeof(AppArgs)));
+                    }
+                    return;
+                }
+            }
+
+            if (!File.Exists(parsedArgs.InputFile))
+            {
+                Console.WriteLine("File [{0}] does not exist", parsedArgs.InputFile);
+                return;
+            }
+
+            try
+            {
+                var config = new SqlBatchReaderConfig()
+                {
+                    TrimWhiteSpace = parsedArgs.TrimWhiteSpace,
+                    RemoveComments = parsedArgs.RemoveComments,
+                    OmitEmptyLines = parsedArgs.OmitEmptyLines
+                };
+
+                using (var batchReader = new SqlBatchReader.SqlBatchReader(parsedArgs.InputFile, config))
+                {
+                    var counter = 0;
+                    foreach (var batch in batchReader.Batches())
+                    {
+                        counter++;
+                        Console.WriteLine("***BATCH--[{0}]***", counter);
+                        Console.WriteLine(batch);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Unhandled exception {0}\n{1}", ex.Message, ex.StackTrace);
+            }
+        }
+    }
+}
